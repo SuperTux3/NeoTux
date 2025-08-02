5d045f61c69e541922b94c4f0271c817aac0e4b4
Swagtoy <me@ow.swag.toys>
2025-08-01 23:25:26 -0400

    Surface blitter

---
+++ src/video/sdl/surface_blitter.cpp
@@ -0,0 +1,68 @@
+//  SuperTux 
+//  Copyright (C) 2025 Hyland B. <me@ow.swag.toys> 
+// 
+//  This program is free software: you can redistribute it and/or modify 
+//  it under the terms of the GNU General Public License as published by 
+//  the Free Software Foundation, either version 3 of the License, or 
+//  (at your option) any later version. 
+// 
+//  This program is distributed in the hope that it will be useful, 
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+//  GNU General Public License for more details. 
+// 
+//  You should have received a copy of the GNU General Public License 
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <SDL3/SDL_pixels.h>
+#include <SDL3/SDL_endian.h>
+#include "surface_blitter.hpp"
+
+SurfaceBlitter::SurfaceBlitter(Size size) :
+	m_sdl_surface(nullptr, SDL_DestroySurface),
+	m_size(std::move(size))
+{
+	m_sdl_surface.reset();
+}
+
+SDL_Color
+SurfaceBlitter::read_pixel(int x, int y)
+{
+	SDL_Color res;
+	SDL_Surface *surface = m_sdl_surface.get();
+	Uint8 *p;
+	p = reinterpret_cast<Uint8*>(surface->pixels) + y * surface->pitch + x * SDL_BYTESPERPIXEL(surface->format);
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	res.r = p[0];
+	res.g = p[1];
+	res.b = p[2];
+	res.a = p[3];
+#else
+	res.r = p[3];
+	res.g = p[2];
+	res.b = p[1];
+	res.a = p[0];
+#endif
+	return res;
+}
+
+void
+SurfaceBlitter::write_pixel(int x, int y, SDL_Color color)
+{
+	SDL_Surface *surface = m_sdl_surface.get();
+	Uint8 *p;
+	p = reinterpret_cast<Uint8*>(surface->pixels) + y * surface->pitch + x * SDL_BYTESPERPIXEL(surface->format);
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	p[0] = color.r;
+	p[1] = color.g;
+	p[2] = color.b;
+	p[3] = color.a;
+#else
+	p[0] = color.a;
+	p[1] = color.b;
+	p[2] = color.g;
+	p[3] = color.r;
+#endif
+	//SDL_WriteSurfacePixel(m_sdl_surface.get(), x, y, color.r, color.g, color.b, color.a);
+}
+
---
+++ src/video/sdl/surface_blitter.hpp
@@ -0,0 +1,38 @@
+//  SuperTux 
+//  Copyright (C) 2025 Hyland B. <me@ow.swag.toys> 
+// 
+//  This program is free software: you can redistribute it and/or modify 
+//  it under the terms of the GNU General Public License as published by 
+//  the Free Software Foundation, either version 3 of the License, or 
+//  (at your option) any later version. 
+// 
+//  This program is distributed in the hope that it will be useful, 
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+//  GNU General Public License for more details. 
+// 
+//  You should have received a copy of the GNU General Public License 
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SUPERTUX_SRC_VIDEO_SDL_SURFACE_BLITTER_HPP
+#define SUPERTUX_SRC_VIDEO_SDL_SURFACE_BLITTER_HPP
+
+#include <SDL3/SDL_pixels.h>
+#include <memory>
+#include <SDL3/SDL_surface.h>
+#include "math/size.hpp"
+
+class SurfaceBlitter
+{
+public:
+	SurfaceBlitter(Size size);
+	~SurfaceBlitter() = default;
+	
+	SDL_Color read_pixel(int x, int y);
+	void write_pixel(int x, int y, SDL_Color color);
+private:
+	std::unique_ptr<SDL_Surface, decltype(&SDL_DestroySurface)> m_sdl_surface;
+	Size m_size;
+};
+
+#endif
