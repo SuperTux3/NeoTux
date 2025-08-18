c447d5af294c97505e9691d3edf7afa4f3a10b62
Swagtoy <me@ow.swag.toys>
2025-08-18 02:32:49 -0400

    Export sprite files

--- scripts/convert2spritesheet.pl
+++ scripts/convert2spritesheet.pl
@@ -46,9 +46,23 @@ die "Must provide two arguments.\n" . helpme . "\n" unless $src && $dest;
 sub src_dir { $src . '/' . $_[0] }
 sub dest_dir { $dest . '/' . $_[0] }
 
-sub append_spritesheet_from_data
+sub cons_action
 {
-	my ($image, $dir, $actions) = @_;
+	my $action = $_[0];
+	my ($name, $fps, $hitbox) = ($action->{'name'}, $action->{'fps'}, $action->{'hitbox'});
+	#print Dumper $images, $hitbox, $fps, $name;
+	my $pad = "\t\t\t";
+	my $res = "$pad(action \n";
+	$res .= "$pad  (name \"$name\")\n" if $name;
+	$res .= "$pad  (fps $fps)\n" unless $fps eq '0';
+	$res .= sprintf("$pad  (hitbox %s %s %s %s)\n", $hitbox->[0], $hitbox->[1], $hitbox->[2], $hitbox->[3]);
+	
+	$res;
+}
+
+sub parse_sprite_data
+{
+	my ($image, $dir, $name, $actions) = @_;
 	my $curr_max_w = 0;
 	my $curr_max_h = 0;
 	my $curr_y = 0;
@@ -57,52 +71,79 @@ sub append_spritesheet_from_data
 	
 	my %used_images;
 	
+	my $sexp = ";; Ugly trash proudly produced by ./convert2spritesheet.pl
+(supertux-sprite\n
+\t\t\t(spritesheet (image \"".$name."_spritesheet.png\"))\n";
+	
 	foreach my $action (@$actions)
 	{
+		my $is_spritesheet = 0;
+		$sexp .= cons_action($action);
+		if (scalar @{$action->{'images'}} != 0)
+		{
+			$is_spritesheet = 1;
+			$sexp .= "\t\t\t  (spritesheet\n";
+		}
 		foreach my $file_rel (@{$action->{'images'}})
 		{
 			my $file = src_dir($dir) . '/' . $file_rel;
-			if ($used_images{$file})
-			{
-				# Don't punch in new images again
-				next;
-			}
-			else {
-				$used_images{$file} = 1;
-			}
+			# Don't punch in new images again
+			#next if ($used_images{$file});
 			my $sprite = Image::Magick->new;
 			
 			# Work with image now
 			my $err = $sprite->Read($file);
 			my ($w, $h) = $sprite->Get('width', 'height');
-			$max_h = $h if $h > $max_h;
-			# Set canvas size
-			$curr_max_h = $h + $curr_y if ($h + $curr_y > $curr_max_h);
-			$curr_max_w = $w + $curr_x if ($w + $curr_x > $curr_max_w);
 			
-			# Resize image width if larger
-			#$image->Extent(width => ($curr_x + $w), height => ($curr_y + $max_h));
-			$image->Extent(geometry=>$curr_max_w.'x'.$curr_max_h, background=>'transparent');
-			
-			$image->CopyPixels(
-				image => $sprite,
-				width => $w, height => $h,
-				x => 0, y => 0,
-				offset => '0x0+'.$curr_x.'+'.$curr_y
-			);
-			
-			# For the next image...
-			undef $sprite;
-			$curr_x += $w;
-			
-			if ($curr_x > $MAX_W)
+			unless ($used_images{$file})
 			{
-				$curr_x = 0;
-				$curr_y += $max_h;
-				$max_h = 0;
+				$max_h = $h if $h > $max_h;
+				# Set canvas size
+				$curr_max_h = $h + $curr_y if ($h + $curr_y > $curr_max_h);
+				$curr_max_w = $w + $curr_x if ($w + $curr_x > $curr_max_w);
+
+				# Resize image width if larger
+				#$image->Extent(width => ($curr_x + $w), height => ($curr_y + $max_h));
+				$image->Extent(geometry=>$curr_max_w.'x'.$curr_max_h, background=>'transparent');
+
+				$image->CopyPixels(
+					image => $sprite,
+					width => $w, height => $h,
+					x => 0, y => 0,
+					offset => '0x0+'.$curr_x.'+'.$curr_y
+				);
+
+				# Store spritesheet information
+				$used_images{$file} = [
+					$curr_x, $curr_y,
+					$w, $h
+				];
+
+				# For the next image...
+				$curr_x += $w;
+
+				if ($curr_x > $MAX_W)
+				{
+					$curr_x = 0;
+					$curr_y += $max_h;
+					$max_h = 0;
+				}
 			}
+			
+			$sexp .= sprintf("\t\t\t    (%d %d %d %d)\n",
+				$used_images{$file}->[0], $used_images{$file}->[1],
+				$used_images{$file}->[2], $used_images{$file}->[3]) if $is_spritesheet;
+			
+			undef $sprite;
 		}
+		$sexp .= ')';
+		$sexp .= ')' if $is_spritesheet;
 	}
+	
+	$sexp .= ')';
+	open my $fh, '>', dest_dir($dir).'/'.$name."_sheet.sprite" or die "Could not open sprite";
+	print $fh $sexp;
+	print "Writing ".dest_dir($dir).'/'.$name."_sheet.sprite\n";
 }
 
 sub parse_sprite
@@ -130,6 +171,9 @@ sub parse_sprite
 		{
 			my $pname;
 			my @images;
+			# For consistency we keep fps as a string.
+			my $fps = "0";
+			my @hitbox;
 			# action
 			foreach my $list (@$root)
 			{
@@ -142,22 +186,31 @@ sub parse_sprite
 						{
 							$next = 1 if $action eq "name";
 							$next = 2 if $action eq "images";
+							$next = 3 if $action eq "hitbox";
+							$next = 4 if $action eq "fps";
 							next;
 						}
 						$pname = $action if $next == 1;
 						push(@images, $action) if $next == 2;
+						push(@hitbox, $action) if $next == 3;
+						$fps = $action if $next == 4;
 					}
 				}
 			}
-			push(@actions, { name => $pname, images => [@images] });
+			push(@actions, {
+				name   => $pname,
+				images => [@images],
+				fps    => $fps,
+				hitbox => [@hitbox],
+			});
 		}
 	}
 	
-	append_spritesheet_from_data($image, $dir, \@actions);
-	my $x = $image->Write(dest_dir($dir) . "/sprite_$name.png");
+	parse_sprite_data($image, $dir, $name, \@actions);
+	my $x = $image->Write(dest_dir($dir) . "/".$name."_spritesheet.png");
 	warn "$x" if "$x";
 	
-	print "Writing ".dest_dir($dir)."/sprite_$name.png\n";
+	print "Writing ".dest_dir($dir)."/".$name."_spritesheet.png\n";
 	undef $image;
 }
 
